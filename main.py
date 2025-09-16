4eba176e38ebbd264ca59b4ef6f8eac01533519e
Pascal Ziener <30428982+pascalziener@users.noreply.github.com>
2025-09-16 21:24:11 +0200

    Add sorting of chars and add using parameters for books

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
-from stats import get_num_words,get_char_count
+import sys
+from stats import get_num_words,get_char_count, get_sorted_char_count
 
 def get_book_text(filepath):
   with open(filepath) as f:
     file_contents = f.read()
     return file_contents
 
-def sort_on(items):
-  return items[1]
+
 
 def main():
-  book_text = get_book_text("books/frankenstein.txt")
-  print(f"{get_num_words(book_text)} words found in the document")
+  if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+  book_text = get_book_text(sys.argv[1])
   char_count = get_char_count(book_text)
+  sorted_char_count = get_sorted_char_count(char_count)
 
-  sorted_char_count = dict(sorted(char_count.items(), key=sort_on, reverse=True))
-
+  print("===================== BOOKBOT =====================")
+  print(f"Analyzing book found at {sys.argv[1]}...")
+  print("------------------- Word Count --------------------")
+  print(f"Found {get_num_words(book_text)} total words")
+  print("----------------- Character Count -----------------")
   for char in sorted_char_count:
-    print(f"'{char}': {sorted_char_count[char]}")
+    print(f"{char["char"]}: {char["num"]}")
 
 main()
--- stats.py
+++ stats.py
@@ -1,7 +1,6 @@
 def get_num_words(text):
   return len(text.split())
 
-
 def get_char_count(text):
   char_count = {}
   for char in text:
@@ -14,3 +13,13 @@ def get_char_count(text):
       char_count[c] = 1
 
   return char_count
+
+def sort_on(items):
+  return items["num"]
+
+def get_sorted_char_count(char_counts):
+  sorted_char_count = list({"char": k, "num": v} for k,v in char_counts.items())
+  sorted_char_count.sort(reverse=True, key=sort_on)
+  return sorted_char_count
+
+
